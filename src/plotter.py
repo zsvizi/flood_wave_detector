7fb76b13ea9b78b16ad7af5a7a331d917179a962
budail <budai.lacko@gmail.com>
2022-07-14 15:32:04 +0200

    Add plotter.py with Plotter class. Move merge_graphs, plot_graph and filter_graph methods to Plotter class.

---
+++ src/plotter.py
@@ -0,0 +1,169 @@
+from datetime import datetime, timedelta
+import os
+
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.readwrite import json_graph
+import numpy as np
+import pandas as pd
+
+from src.flood_wave_detector import FloodWaveDetector
+from src.json_helper import JsonHelper
+
+
+class Plotter:
+    def __init__(self, fwd: FloodWaveDetector) -> None:
+        self.fwd = fwd
+
+    def merge_graphs(self, start_station: int, end_station: int, start_date: str, end_date: str,
+                     span: bool, hs: int, he: int, vs, ve):
+
+        joined_graph = self.fwd.filter_graph(start_station=start_station, end_station=end_station,
+                                             start_date=start_date, end_date=end_date)[0]
+
+        joined_graph_save = nx.node_link_data(joined_graph)
+        JsonHelper.write(filepath=f'./saved/merge_graphs.json', obj=joined_graph_save, log=False)
+
+        start = datetime.strptime(start_date, '%Y-%m-%d')
+        end = datetime.strptime(end_date, '%Y-%m-%d')
+
+        positions = dict()
+        for node in joined_graph.nodes():
+            x_coord = abs((start - datetime.strptime(node[1], '%Y-%m-%d')).days) - 1
+            y_coord = len(self.fwd.gauges) - self.fwd.gauges.index(int(node[0]))
+            positions[node] = (x_coord, y_coord)
+
+        fig, ax = plt.subplots()
+        if span:
+            ax.axhspan(vs, ve, color='green', alpha=0.3, label="Window for maximum")
+            ax.axvspan(hs, he, color='orange', alpha=0.3, label="Window for maximum")
+        min_x = -1
+        max_x = max([n[0] for n in positions.values()])
+        x_labels = pd.date_range(start - timedelta(days=1),
+                                 start + timedelta(days=max_x + 1),
+                                 freq='d').strftime('%Y-%m-%d').tolist()
+        ax.xaxis.set_ticks(np.arange(min_x - 1, max_x + 1, 1))
+        ax.set_xticklabels(x_labels, rotation=20, horizontalalignment='right', fontsize=102)
+
+        min_y = 1
+        max_y = len(self.fwd.gauges) + 1
+        y_labels = [str(gauge) for gauge in self.fwd.gauges[::-1]]
+        ax.yaxis.set_ticks(np.arange(min_y, max_y, 1))
+        ax.set_yticklabels(y_labels, rotation=20, horizontalalignment='right', fontsize=32)
+
+        plt.rcParams["figure.figsize"] = (40, 10)
+        nx.draw(joined_graph, pos=positions, node_size=1000)
+        plt.axis('on')  # turns on axis
+        ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+        plt.savefig('graph_.pdf')
+
+    def plot_graph(self, start: str, end: str):
+
+        if self.fwd.gauge_peak_plateau_pairs == {}:
+            self.fwd.gauge_peak_plateau_pairs = JsonHelper.read(filepath='./saved/step2/gauge_peak_plateau_pairs.json',
+                                                                log=False)
+
+        self.fwd.gauge_pairs = list(self.fwd.gauge_peak_plateau_pairs.keys())
+
+        joined_graph = nx.DiGraph()
+        for gauge_pair in self.fwd.gauge_pairs:
+            filenames = next(os.walk(f'./saved/step3/{gauge_pair}'), (None, None, []))[2]
+            sorted_files = self.fwd.sort_wave(filenames=filenames, start=start, end=end)
+            for file in sorted_files:
+                data = JsonHelper.read(filepath=f'./saved/step3/{gauge_pair}/{file}', log=False)
+                h = json_graph.node_link_graph(data)
+                # Read a file and load it
+                joined_graph = nx.compose(joined_graph, h)
+
+        joined_graph_save = nx.node_link_data(joined_graph)
+        JsonHelper.write(filepath=f'./saved/plot_graph.json', obj=joined_graph_save, log=False)
+
+        start = datetime.strptime(start, '%Y-%m-%d')
+        end = datetime.strptime(end, '%Y-%m-%d')
+
+        positions = dict()
+        for node in joined_graph.nodes():
+            x_coord = abs((start - datetime.strptime(node[1], '%Y-%m-%d')).days) - 1
+            y_coord = len(self.fwd.gauges) - self.fwd.gauges.index(int(node[0]))
+            positions[node] = (x_coord, y_coord)
+
+        fig, ax = plt.subplots()
+        ax.axhspan(4, 9, color='green', alpha=0.3, label="Window for maximum")
+        min_x = -1
+        max_x = max([n[0] for n in positions.values()])
+
+        x_labels = pd.date_range(start - timedelta(days=1),
+                                 start + timedelta(days=max_x + 1),
+                                 freq='d').strftime('%Y-%m-%d').tolist()
+        ax.xaxis.set_ticks(np.arange(min_x - 1, max_x + 1, 1))
+        ax.set_xticklabels(x_labels, rotation=20, horizontalalignment='right', fontsize=15)
+
+        min_y = 1
+        max_y = len(self.fwd.gauges) + 1
+        y_labels = [str(gauge) for gauge in self.fwd.gauges[::-1]]
+        ax.yaxis.set_ticks(np.arange(min_y, max_y, 1))
+        ax.set_yticklabels(y_labels, rotation=20, horizontalalignment='right', fontsize=22)
+
+        plt.rcParams["figure.figsize"] = (30, 20)
+        nx.draw(joined_graph, pos=positions, node_size=500)
+        limits = plt.axis('on')  # turns on axis
+        ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+        plt.savefig('graph.pdf')
+
+    def filter_graph(self, start_station: int, end_station: int, start_date: str, end_date: str):
+
+        if self.fwd.gauge_peak_plateau_pairs == {}:
+            self.fwd.gauge_peak_plateau_pairs = JsonHelper.read(filepath='./saved/step2/gauge_peak_plateau_pairs.json',
+                                                                log=False)
+
+        # first filter
+        self.fwd.gauge_pairs = list(self.fwd.gauge_peak_plateau_pairs.keys())
+        up_limit = self.fwd.meta.loc[start_station].river_km
+        low_limit = self.fwd.meta.loc[end_station].river_km
+        selected_meta = self.fwd.meta[(self.fwd.meta['river_km'] >= low_limit)]
+        start_gauges = selected_meta.dropna(subset=['h_table']).index.tolist()
+
+        selected_pairs = [x for x in self.fwd.gauge_pairs if int(x.split('_')[0]) in start_gauges]
+
+        joined_graph = nx.Graph()
+        for gauge_pair in selected_pairs:
+            filenames = next(os.walk(f'./saved/step3/{gauge_pair}'), (None, None, []))[2]
+            sorted_files = self.fwd.sort_wave(filenames=filenames, start=start_date, end=end_date)
+            for file in sorted_files:
+                data = JsonHelper.read(filepath=f'./saved/step3/{gauge_pair}/{file}', log=False)
+                h = json_graph.node_link_graph(data)
+                # Read a file and load it
+                joined_graph = nx.compose(joined_graph, h)
+
+        selected_meta = self.fwd.meta[(self.fwd.meta['river_km'] >= low_limit) &
+                                      (self.fwd.meta['river_km'] <= up_limit)]
+        comp_gauges = selected_meta.dropna(subset=['h_table']).index.tolist()
+        # second filter
+        comp = [x for x in self.fwd.gauges if x not in comp_gauges]
+        remove = [x for x in joined_graph.nodes if int(x[0]) in comp]
+        print(joined_graph.nodes)
+        joined_graph.remove_nodes_from(remove)
+        print(joined_graph.nodes)
+
+        # third filter
+        remove_date = [x for x in joined_graph.nodes if ((x[1] > end_date) or (x[1] < start_date))]
+        joined_graph.remove_nodes_from(remove_date)
+        print(joined_graph.nodes)
+
+        # fourth filter
+        cc = [list(x) for x in nx.connected_components(joined_graph)]
+        for sub_cc in cc:
+            res_start = [int(node[0]) == start_station for node in sub_cc]
+            res_end = [int(node[0]) == end_station for node in sub_cc]
+            if (True not in res_start) or (True not in res_end):
+                joined_graph.remove_nodes_from(sub_cc)
+        total_waves = 0
+        for sub_cc in cc:
+            start_nodes = [node for node in sub_cc if int(node[0]) == start_station]
+            end_nodes = [node for node in sub_cc if int(node[0]) == end_station]
+            for start in start_nodes:
+                for end in end_nodes:
+                    paths = [list(x) for x in nx.all_shortest_paths(joined_graph, source=start, target=end)]
+                    total_waves += len(paths)
+
+        return joined_graph, total_waves
