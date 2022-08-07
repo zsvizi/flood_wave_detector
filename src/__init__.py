36bbcc2c1e8dd31fddd1a06a69a40a72781cdad7
budail <budai.lacko@gmail.com>
2022-08-07 09:42:00 +0200

    Clean __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,3 @@
 import os
+
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-import sys
-sys.path.append(PROJECT_PATH)
-print(sys.path)
-from src.flood_wave_detector import FloodWaveDetector
