6a709db48b30db449f18f23ab5dd585213c5f7c1
Dabis Marcell <dabis.marcell529@gmail.com>
2022-08-11 11:02:09 +0200

    Add to requirements.txt and dataloader.py

---
+++ src/dataloader.py
@@ -0,0 +1,19 @@
+from data_ativizig.dataloader import Dataloader
+
+from google.cloud import storage
+import os
+
+
+class CombinedDataloader:
+    def __init__(self):
+        self.__db_credentials_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.ini')
+        self.dataloader = Dataloader(self.__db_credentials_path)
+        self.meta = self.dataloader.meta_data \
+            .groupby(["river"]) \
+            .get_group("Tisza") \
+            .sort_values(by='river_km', ascending=False)
+        self.gauges = self.meta.dropna(subset=['h_table']).index.tolist()
+
+    def get_files_from_bucket(self):
+        storage_client = storage.Client
+        pass
