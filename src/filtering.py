7f8d7575aedacd2bc90cfa389896e2bcbed8af80
Viktor <h048686@@stud.u-szeged.hu>
2024-02-11 15:54:59 +0100

    height is now a variable of filter_by_water_level

--- src/filtering.py
+++ src/filtering.py
@@ -76,7 +76,12 @@ class Filtering:
 
         return g
 
-    def filter_by_water_level(self, graph: nx.DiGraph, gauge: str, positions: dict, node_colors: list) -> nx.DiGraph:
+    def filter_by_water_level(self,
+                              graph: nx.DiGraph,
+                              gauge: str,
+                              positions: dict,
+                              node_colors: list,
+                              height: str) -> nx.DiGraph:
         """
         This function filters out weakly connected components that have high water level at the given gauge.
 
@@ -84,8 +89,16 @@ class Filtering:
         :param str gauge: gauge number to be filtered by as a string
         :param positions: positions of the graph
         :param node_colors: colors of the nodes of the graph; yellow if the water level is low, red if it's high
+        :param str height: level group; either "high" or "low"
         :return nx.DiGraph: graph that contains only components that have high water level at the given gauge
         """
+
+        if height == "high":
+            c = "red"
+        elif height == "low":
+            c = "yellow"
+        else:
+            raise Exception("Height can be either high or low.")
 
         comps = list(nx.weakly_connected_components(graph))
         edges = graph.edges()
@@ -108,7 +121,7 @@ class Filtering:
                     idx = list(positions.keys()).index(elem)
                     colors_of_gauge.append(node_colors[idx])
 
-                if not any("red" == color for color in colors_of_gauge):
+                if not any(c == color for color in colors_of_gauge):
                     comps.remove(comp)
 
         nodes_filtered, edges_filtered = self.nodes_and_edges(comps=comps, edges=edges)
