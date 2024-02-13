b9189e61499cc8396ef3f7de33de8e73d02c80ea
Viktor <h048686@@stud.u-szeged.hu>
2024-02-13 17:06:02 +0100

    helper functions were moved to filtering_handler.py

--- src/filtering.py
+++ src/filtering.py
@@ -1,5 +1,7 @@
 import networkx as nx
 
+from filtering_handler import FilteringHandler
+
 
 class Filtering:
     """
@@ -23,10 +25,10 @@ class Filtering:
 
         comps_new = []
         for comp in comps_copy:
-            if Filtering.is_gauge_in_comp(gauge=gauge, comp_list=list(comp)):
+            if FilteringHandler.is_gauge_in_comp(gauge=gauge, comp_list=list(comp)):
                 comps_new.append(comp)
 
-        nodes_filtered, edges_filtered = Filtering.nodes_and_edges(comps=comps_new, edges=edges)
+        nodes_filtered, edges_filtered = FilteringHandler.nodes_and_edges(comps=comps_new, edges=edges)
 
         g = nx.DiGraph()
         g.add_nodes_from(nodes_filtered)
@@ -54,15 +56,15 @@ class Filtering:
         edges = filtered.edges()
         edges = list(edges)
 
-        gauges = Filtering.get_gauges(comps=comps)
+        gauges = FilteringHandler.get_gauges(comps=comps)
 
         gauges_to_delete = gauges[:gauges.index(start_gauge)]
-        comps = Filtering.remove_nodes(comps=comps, gauges_to_delete=gauges_to_delete)
+        comps = FilteringHandler.remove_nodes(comps=comps, gauges_to_delete=gauges_to_delete)
 
         gauges_to_delete = gauges[gauges.index(end_gauge):]
-        comps = Filtering.remove_nodes(comps=comps, gauges_to_delete=gauges_to_delete)
+        comps = FilteringHandler.remove_nodes(comps=comps, gauges_to_delete=gauges_to_delete)
 
-        nodes_filtered, edges_filtered = Filtering.nodes_and_edges(comps=comps, edges=edges)
+        nodes_filtered, edges_filtered = FilteringHandler.nodes_and_edges(comps=comps, edges=edges)
 
         g = nx.DiGraph()
         g.add_nodes_from(nodes_filtered)
@@ -87,7 +89,7 @@ class Filtering:
         comps_copy = comps.copy()
         edges = list(edges)
 
-        gauges = Filtering.get_gauges(comps=comps)
+        gauges = FilteringHandler.get_gauges(comps=comps)
 
         filtered_gauges = gauges[gauges.index(start_gauge):gauges.index(end_gauge) + 1]
 
@@ -95,57 +97,19 @@ class Filtering:
         for comp in comps_copy:
             list_of_bools = []
             for fg in filtered_gauges:
-                x = Filtering.is_gauge_in_comp(gauge=fg, comp_list=list(comp))
+                x = FilteringHandler.is_gauge_in_comp(gauge=fg, comp_list=list(comp))
                 list_of_bools.append(x)
 
             if any(list_of_bools):
                 comps_new.append(comp)
 
-        nodes_filtered, edges_filtered = Filtering.nodes_and_edges(comps=comps_new, edges=edges)
+        nodes_filtered, edges_filtered = FilteringHandler.nodes_and_edges(comps=comps_new, edges=edges)
 
         g = nx.DiGraph()
         g.add_nodes_from(nodes_filtered)
         g.add_edges_from(edges_filtered)
 
         return g
-
-    @staticmethod
-    def get_gauges(comps: list) -> list:
-        """
-        This function collects the gauges corresponding to a node in the actual graph
-        :param list comps: list of components in the graph
-        :return list: decreasingly sorted gauge numbers
-        """
-
-        for i in range(len(comps)):
-            comps[i] = list(comps[i])
-        nodes = [item for sublist in comps for item in sublist]
-
-        gauges = []
-        for node in nodes:
-            if node[0] not in gauges:
-                gauges.append(node[0])
-        decreasing_gauges = sorted(gauges, key=lambda x: str(float(x)), reverse=True)
-
-        return decreasing_gauges
-
-    @staticmethod
-    def remove_nodes(comps: list, gauges_to_delete: list) -> list:
-        """
-        This function removes nodes corresponding to gauges_to_delete from all components.
-
-        :param list comps: list of components
-        :param list gauges_to_delete: list of gauges to delete
-        :return list: remaining components
-        """
-
-        comps_copy = comps.copy()
-        for comp in comps_copy:
-            comp_copy = comp.copy()
-            for elem in comp_copy:
-                if any(gtd == elem[0] for gtd in gauges_to_delete):
-                    comps[comps.index(comp)].remove(elem)
-        return comps
 
     @staticmethod
     def filter_by_water_level(graph: nx.DiGraph,
@@ -195,48 +159,10 @@ class Filtering:
                 if not any(c == color for color in colors_of_gauge):
                     comps.remove(comp)
 
-        nodes_filtered, edges_filtered = Filtering.nodes_and_edges(comps=comps, edges=edges)
+        nodes_filtered, edges_filtered = FilteringHandler.nodes_and_edges(comps=comps, edges=edges)
 
         g = nx.DiGraph()
         g.add_nodes_from(nodes_filtered)
         g.add_edges_from(edges_filtered)
 
         return g
-
-    @staticmethod
-    def is_gauge_in_comp(gauge: str, comp_list: list) -> bool:
-        """
-        This function checks whether the weakly connected component comp_list has a node at gauge.
-
-        :param str gauge: given gauge number as a string
-        :param list comp_list: given weakly connected component as a list
-        :return bool: True if the gauge is in the component, False otherwise
-        """
-
-        return any(gauge == elem for elem in [i[0] for i in [list(ele) for ele in comp_list]])
-
-    @staticmethod
-    def nodes_and_edges(comps: list, edges: list):
-        """
-        This function finds and collects the nodes and edges of the filtered graph.
-
-        :param list comps: list of the components
-        :param list edges: list of the edges
-        :return: list of nodes and list of edges
-        """
-
-        for i in range(len(comps)):
-            comps[i] = list(comps[i])
-        nodes = [item for sublist in comps for item in sublist]
-
-        edges_to_keep = []
-        for edge in edges:
-            found_in_comp = False
-            for comp in comps:
-                if edge[0] in comp:
-                    found_in_comp = True
-                    break
-            if found_in_comp:
-                edges_to_keep.append(edge)
-
-        return nodes, edges_to_keep
---
+++ src/filtering_handler.py
@@ -0,0 +1,80 @@
+class FilteringHandler:
+    """
+    This is a helper class for Filtering
+    """
+
+    @staticmethod
+    def get_gauges(comps: list) -> list:
+        """
+        This function collects the gauges corresponding to a node in the actual graph
+        :param list comps: list of components in the graph
+        :return list: decreasingly sorted gauge numbers
+        """
+
+        for i in range(len(comps)):
+            comps[i] = list(comps[i])
+        nodes = [item for sublist in comps for item in sublist]
+
+        gauges = []
+        for node in nodes:
+            if node[0] not in gauges:
+                gauges.append(node[0])
+        decreasing_gauges = sorted(gauges, key=lambda x: str(float(x)), reverse=True)
+
+        return decreasing_gauges
+
+    @staticmethod
+    def remove_nodes(comps: list, gauges_to_delete: list) -> list:
+        """
+        This function removes nodes corresponding to gauges_to_delete from all components.
+
+        :param list comps: list of components
+        :param list gauges_to_delete: list of gauges to delete
+        :return list: remaining components
+        """
+
+        comps_copy = comps.copy()
+        for comp in comps_copy:
+            comp_copy = comp.copy()
+            for elem in comp_copy:
+                if any(gtd == elem[0] for gtd in gauges_to_delete):
+                    comps[comps.index(comp)].remove(elem)
+        return comps
+
+    @staticmethod
+    def is_gauge_in_comp(gauge: str, comp_list: list) -> bool:
+        """
+        This function checks whether the weakly connected component comp_list has a node at gauge.
+
+        :param str gauge: given gauge number as a string
+        :param list comp_list: given weakly connected component as a list
+        :return bool: True if the gauge is in the component, False otherwise
+        """
+
+        return any(gauge == elem for elem in [i[0] for i in [list(ele) for ele in comp_list]])
+
+    @staticmethod
+    def nodes_and_edges(comps: list, edges: list):
+        """
+        This function finds and collects the nodes and edges of the filtered graph.
+
+        :param list comps: list of the components
+        :param list edges: list of the edges
+        :return: list of nodes and list of edges
+        """
+
+        for i in range(len(comps)):
+            comps[i] = list(comps[i])
+        nodes = [item for sublist in comps for item in sublist]
+
+        edges_to_keep = []
+        for edge in edges:
+            found_in_comp = False
+            for comp in comps:
+                if edge[0] in comp:
+                    found_in_comp = True
+                    break
+            if found_in_comp:
+                edges_to_keep.append(edge)
+
+        return nodes, edges_to_keep
