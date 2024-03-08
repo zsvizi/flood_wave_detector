73f6c82be166177424e2caeed55e714bf199bb46
Viktor <h048686@@stud.u-szeged.hu>
2024-03-08 14:06:50 +0100

    count_unfinished_waves updated

--- src/analysis/graph_analysis.py
+++ src/analysis/graph_analysis.py
@@ -206,7 +206,6 @@ class GraphAnalysis:
 
     @staticmethod
     def count_unfinished_waves(joined_graph: nx.DiGraph,
-                               gauges: list,
                                start_station: str,
                                end_station: str
                                ) -> int:
@@ -215,79 +214,28 @@ class GraphAnalysis:
         If there were branching(s), then all the branches will be counted.
 
         :param nx.DiGraph joined_graph: full composed graph of the desired time interval
-        :param list gauges: list of gauges
         :param str start_station: The ID of the desired start station
         :param str end_station: The ID of the last station, which is not reached by the flood waves
         :return int: The number of flood waves which impacted the start_station but did not reach the end_station
         """
-        # Some used nx features are not implemented for DiGraphs
-        joined_graph = joined_graph.to_undirected()
-        # First we select the gauges between start_station and end_station
-        start_index = gauges.index(start_station)
-        end_index = gauges.index(end_station)
-        gauges = gauges[start_index:end_index + 1]
 
-        # We select the nodes of the graph, where the gauge (node[0]) is in the already existing gauges list
-        nodes = [
-            node
-            for node in joined_graph.nodes
-            if float(node[0]) in gauges
-        ]
-        # Creating the subgraph induced on the nodes list
-        subgraph = joined_graph.subgraph(nodes)
-        
-        # We need the connected components of subgraph, but the components must have at least two vertices
-        connected_components = [
-            list(x)
-            for x in nx.connected_components(subgraph)
-            if len(list(x)) >= 2
-        ]
-       
-        unfinished_waves = 0
-            
-        # We iterate through every connected component of subgraph
-        for sub_connected_component in connected_components:
+        select_all_in_interval = Selection.select_only_in_interval(joined_graph=joined_graph,
+                                                                   start_station=start_station,
+                                                                   end_station=end_station)
 
-            # If the gauge (node[0]) of a node is the start station, we will count waves from that node
-            start_nodes = [
-                node
-                for node in sub_connected_component
-                if node[0] == start_station
-            ]
+        classes = GraphAnalysis.get_flood_waves_without_equivalence(joined_graph=select_all_in_interval)
 
-            # We need to select which gauges are included in the connected component
-            component_gauges = [
-                x[0]
-                for x in sub_connected_component
-            ]
-            
-            # Ordering the component's gauges with respect to river km
-            component_gauges_ordered = [
-                str(x)
-                for x in gauges
-                if str(x) in component_gauges
-            ]
-            # A node is end node if its gauge (node[0]) is the last element of the ordered list
-            end_nodes = [
-                node
-                for node in sub_connected_component
-                if node[0] == component_gauges_ordered[-1]
-            ]
-            
-            # Counting the number of waves between all start and end nodes
-            for start_node in start_nodes:
-                for end_node in end_nodes:
+        final_flood_waves = []
+        for paths in classes:
+            for path in paths:
+                stations = []
+                for node in path:
+                    stations.append(node[0])
 
-                    paths = [
-                        list(x)
-                        for x in nx.all_shortest_paths(joined_graph, source=start_node, target=end_node)
-                    ]
-                    
-                    # We need only those waves, when the last station is not the end station (a. k. a. unfinished wave)
-                    if end_node[0] != end_station:
-                        unfinished_waves += len(paths)
+                if any(start_station == elem for elem in stations) and all(end_station != elem for elem in stations):
+                    final_flood_waves.append(path)
 
-        return unfinished_waves
+        return len(final_flood_waves)
 
     @staticmethod
     def create_flood_map(joined_graph: nx.DiGraph,
